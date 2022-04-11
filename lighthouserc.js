7a352fa46dc73728d6d3d12959ed57b8064a9540
Michael Ng <michaelng270@gmail.com>
2022-04-11 13:57:48 -0400

    Initial commit

---
+++ lighthouserc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  ci: {
+    collect: {
+      url: ['https://www.americastestkitchen.com/'],
+    },
+    upload: {
+      target: 'temporary-public-storage',
+    },
+  },
+};

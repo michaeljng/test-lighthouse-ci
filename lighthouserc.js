9343a6e38416a0bc69c7fe403f31b0101dcedce6
Michael Ng <michaelng270@gmail.com>
2022-04-11 16:31:40 -0400

    Update checked pages

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,12 +1,12 @@
 module.exports = {
   ci: {
     collect: {
-      url: ['https://www.americastestkitchen.com/'],
+      url: ['https://www.americastestkitchen.com/', 'https://www.cookscountry.com/', 'https://www.cooksillustrated.com/', 'https://www.americastestkitchen.com/reviews', 'https://www.americastestkitchen.com/articles', 'https://www.americastestkitchen.com/recipes/13333-cranberry-curd-tart-with-almond-crust'],
     },
     upload: {
       target: 'lhci',
       serverBaseUrl: 'https://mn-lighthouse-ci-61324.herokuapp.com',
-      token: process.env.LHCI_BUILD_TOKEN,
+      token: '377464fe-7164-4f27-8957-0bbdbe2241c5',
     },
   },
 };

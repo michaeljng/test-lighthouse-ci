33d5521c250ff454ca990f5af9bf3d504d0e4118
Michael Ng <michaelng270@gmail.com>
2022-05-09 15:23:23 -0400

    20220509_test7

--- puppeteer-script.js
+++ puppeteer-script.js
@@ -11,17 +11,15 @@ module.exports = async (browser, context) => {
   await page.click('.login-form__submit');
   await page.waitForNavigation();
   await page.goto('https://www-staging3.americastestkitchen.com/sign_in');
-  await page.authenticate(process.env.LIGHTHOUSE_CHECK_USERNAME, process.env.LIGHTHOUSE_CHECK_PASSWORD);
+  await page.authenticate({'username':process.env.LIGHTHOUSE_CHECK_USERNAME, 'password': process.env.LIGHTHOUSE_CHECK_PASSWORD})
   await page.type('#email', 'puppeteertest@americastestkitchen.com');
   await page.type('.login-form__password', 'password');
   await page.click('.login-form__submit');
   await page.waitForNavigation();
   await page.goto('https://www-staging3.cookscountry.com/sign_in');
-  await page.authenticate(process.env.LIGHTHOUSE_CHECK_USERNAME, process.env.LIGHTHOUSE_CHECK_PASSWORD);
-  await page.waitForNavigation();
+  await page.authenticate({'username':process.env.LIGHTHOUSE_CHECK_USERNAME, 'password': process.env.LIGHTHOUSE_CHECK_PASSWORD})
   await page.goto('https://www-staging3.cooksillustrated.com/sign_in');
-  await page.authenticate(process.env.LIGHTHOUSE_CHECK_USERNAME, process.env.LIGHTHOUSE_CHECK_PASSWORD);
-  await page.waitForNavigation();
+  await page.authenticate({'username':process.env.LIGHTHOUSE_CHECK_USERNAME, 'password': process.env.LIGHTHOUSE_CHECK_PASSWORD})
   // close session for next run
   await page.close();
 };
